ad679d4668e2ce27d0ad3a109eb1677e4114a374
wesley <wesleymotabr@gmail.com>
2019-09-03 15:54:09 -0300

    +Solução do problema do CodeDeploy

--- dependencies/after.sh
+++ dependencies/after.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Set permissions to storage and bootstrap cache
 sudo chmod -R 0777 /var/www/html/storage
+sudo chmod -R 0777 /var/www/html/storage/framework/cache/data
 sudo chmod -R 0777 /var/www/html/bootstrap/cache
 #
 cd /var/www/html
