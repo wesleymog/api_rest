7bade6e0b4f56745a089d3e3603ce99571988a0c
wesley <wesleymotabr@gmail.com>
2019-11-18 15:29:20 -0300

    Alterações no flow principal do pipeline

--- dependencies/before.sh
+++ dependencies/before.sh
@@ -5,3 +5,5 @@ sudo chmod -R 0777 /var/www/html/bootstrap/cache
 sudo cp oauth-private.key /tmp/
 sudo cp oauth-public.key /tmp/
 sudo rm -rf /var/www/html/storage/*
+sudo rm -rf /var/www/html/bootstrap/*
+
