b157e117370817dd233bfe7ec865ce472c38ad42
wesley <wesleymotabr@gmail.com>
2019-11-18 15:26:59 -0300

    Alterações no flow principal do pipeline

--- dependencies/before.sh
+++ dependencies/before.sh
@@ -2,5 +2,6 @@
 # Set permissions to storage and bootstrap cache
 sudo chmod -R 0777 /var/www/html/storage
 sudo chmod -R 0777 /var/www/html/bootstrap/cache
-sudo rm -rf /var/www/html/storage/framework/*
-sudo rm -rf /var/www/html/storage/logs/*
+sudo cp oauth-private.key /tmp/
+sudo cp oauth-public.key /tmp/
+sudo rm -rf /var/www/html/storage/*
