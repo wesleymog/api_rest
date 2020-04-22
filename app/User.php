00fda9acd14af7716ddf8ef9128a6e598fa6a603
api_laravel-at-978029349661 <api_laravel-at-978029349661>
2020-04-22 11:02:22 -0300

    +tentativa de consertar first_access'

--- app/User.php
+++ app/User.php
@@ -192,7 +192,7 @@ class User extends Authenticatable
         if($request->phone_number!=null)$this->phone_number = $request->phone_number;
         if($request->education!=null)$this->education = $request->education;
         if($request->education_institute!=null)$this->education_institute = $request->education_institute;
-        if($request->first_access==0 || $request->first_access==1)$this->first_access = $request->first_access;
+        if($request->first_access===0 || $request->first_access==1)$this->first_access = $request->first_access;
         if($request->profile_picture!=null)$this->profile_picture = $request->profile_picture;
 
         $this->update();
